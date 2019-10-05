735820dff989df2529d53b93f2da8ad92ae601f1
sAnjali12 <anjalisen060@gmail.com>
2019-10-05 22:16:05 +0530

    route of sign in

--- basic_authentication.js
+++ basic_authentication.js
@@ -30,6 +30,25 @@ app.post("/signUp",function(req,res){
 
 })
 
+// GET data
+
+app.get("/get/login/:email/:password",function(req,res){
+    let email = req.params.email
+    let password = req.params.password
+    var data = fs.readFileSync("loginData.json") 
+    var Data = JSON.parse(data);
+    for (index in Data){
+        if(Data[index].emailId=== email && Data[index].password == password){
+            res.send("-------success------")
+            
+        }else{
+            res.send("oops!!! something went's to wrong")
+        }
+
+    }
+})
+
+
 const port = 8000
 app.listen(port,()=>
    console.log(`my server is listning port....${port}`)
